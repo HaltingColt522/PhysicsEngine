577dc781a219a52214a5d835191337ab16ef7579
Til Schaller <schaller.til@gmail.com>
2024-05-12 22:44:08 +0200

    fixed runtime error

--- header/const.h
+++ header/const.h
@@ -3,6 +3,11 @@
 #include <main.h>
 
 namespace Const {
-	const std::vector<const char*> validationLayers;
-	const std::vector<const char*> deviceExtensions;
+    const std::vector<const char*> validationLayers = {
+        "VK_LAYER_KHRONOS_validation"
+    };
+
+    const std::vector<const char*> deviceExtensions = {
+        VK_KHR_SWAPCHAIN_EXTENSION_NAME
+    };
 }
