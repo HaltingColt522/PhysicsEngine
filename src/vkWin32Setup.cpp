24fc8863703b52406d885a6083ec5f87f0438448
HaltingColt522 <l.frotschi@web.de>
2024-07-06 20:57:36 +0200

    win32 surface added

--- src/vkWin32Setup.cpp
+++ src/vkWin32Setup.cpp
@@ -1,5 +1,14 @@
 #include <vkSetup.h>
 
 void Vulkan::winsetup(WINDOW* window, VkInstance instance) {
-	// Vulkan KHR surface //
+	HINSTANCE hinst = GetModuleHandleA(nullptr);
+
+	VkSurfaceKHR surface;
+
+	VkWin32SurfaceCreateInfoKHR surfaceCreateInfo;
+	surfaceCreateInfo.sType = VK_STRUCTURE_TYPE_WIN32_SURFACE_CREATE_INFO_KHR;
+	surfaceCreateInfo.pNext = NULL;
+	surfaceCreateInfo.flags = 0;
+	surfaceCreateInfo.hinstance = hinst;
+	surfaceCreateInfo.hwnd = window->handle;
 }
