17d431c04eea1f52c712546d930bdba5c5de629a
Til Schaller <schaller.til@gmail.com>
2024-07-06 21:08:15 +0200

    vk surface

--- src/vkWin32Setup.cpp
+++ src/vkWin32Setup.cpp
@@ -1,14 +1,23 @@
 #include <vkSetup.h>
 
 void Vulkan::winsetup(WINDOW* window, VkInstance instance) {
-	HINSTANCE hinst = GetModuleHandleA(nullptr);
+
+	//creating VKSURFACE //
 
 	VkSurfaceKHR surface;
 
-	VkWin32SurfaceCreateInfoKHR surfaceCreateInfo;
+	VkWin32SurfaceCreateInfoKHR surfaceCreateInfo = {};
 	surfaceCreateInfo.sType = VK_STRUCTURE_TYPE_WIN32_SURFACE_CREATE_INFO_KHR;
 	surfaceCreateInfo.pNext = NULL;
-	surfaceCreateInfo.flags = 0;
-	surfaceCreateInfo.hinstance = hinst;
+	surfaceCreateInfo.flags = NULL;
+	surfaceCreateInfo.hinstance = GetModuleHandle(NULL);
 	surfaceCreateInfo.hwnd = window->handle;
+
+	if (vkCreateWin32SurfaceKHR(instance, &surfaceCreateInfo, NULL, &surface) != VK_SUCCESS) {
+		throw std::runtime_error("failed to create VkSurface for win32 window");
+	}
+
+	window->surface = surface;
+
+	//picking physical device//
 }
